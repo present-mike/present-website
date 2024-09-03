eca20aac7d3e24bea7cb17fe44159c35db7f92ac
Richard Zhou <richard@activetheory.net>
2024-09-03 01:09:02 -0700

    add static fallback for mobile + shit gpu's

--- src/shaders/HeroSection.jsx
+++ src/shaders/HeroSection.jsx
@@ -2,7 +2,9 @@ import { useRef, } from 'react'
 import { useMediaQuery } from 'react-responsive'
 import PropTypes from 'prop-types'
 import { ScrollScene, UseCanvas } from '@14islands/r3f-scroll-rig';
+import { useDetectGPU } from '@react-three/drei';
 import ReactPlayer from 'react-player/lazy'
+
 import classes from '../routes/home/home.module.css';
 import FadeIn from '../components/fadeIn/FadeIn'
 
@@ -14,36 +16,38 @@ HeroSection.propTypes = {
 }
 
 export default function HeroSection({ src }) {
+    const GPUTier = useDetectGPU();
     const el = useRef();
-
-    const isMobile = true;//useMediaQuery({ query: '(max-width: 425px)' })
 
     return (
         <>
+            {(GPUTier.tier === "0" || (GPUTier.isMobile && GPUTier.tier === "1")) ?
+            <div className={`${classes.screenHeightContainer} ${classes.noTouch}`}>
+                <ReactPlayer
+                    playing
+                    loop
+                    muted
+                    url={src}
+                    key={src}
+                    width='100%'
+                    height='100%'
+                    style={{maxWidth: `100vw`, maxHeight: `100vh`, position: `sticky`, top: 0 }}
+                />
+            </div> :
+
+            <>
             <div ref={el} className={`${classes.screenHeightContainer} ${classes.noTouch}`} />
-            {/* {isMobile ? (
-                    <div className={`${classes.screenHeightContainer} ${classes.noTouch}`}>
-                        <ReactPlayer
-                            playing
-                            loop
-                            muted
-                            url={src}
-                            key={src}
-                            width="100%"
-                            height="100%"
-                        />
-                    </div>
-            ) : ( */}
-                <UseCanvas>
-                    <ScrollScene track={el}>
-                        {
-                            (props) => (
-                                <HeroQuad src={src} {...props} />
-                        )}
-                    </ScrollScene >
-                </UseCanvas >
-            {/* )
-            } */}
+
+            <UseCanvas>
+                <ScrollScene track={el}>
+                    {
+                        (props) => (
+                            <HeroQuad src={src} {...props} />
+                    )}
+                </ScrollScene >
+            </UseCanvas >
+            </>}
+
             <FadeIn />
         </>
     );
